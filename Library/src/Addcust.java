2e14c227253025697aece34e755b4b499e61fc99
jacobpjorgensen <jacobpjorgensen@gmail.com>
2013-11-09 16:27:13 -0700

    Fixed the awful code for addBook class and Addcust class (I don't get the capitalization discrepencies either... :D) Anyway fixed all the GUI problems in both and made them consistent with each other. Cleaned up the code as well. It was not so great...and that's coming from a poor coder :P

--- Library/src/Addcust.java
+++ Library/src/Addcust.java
@@ -2,148 +2,156 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.io.*;
-import java.applet.AppletContext.*;
 import java.util.*;
-import javax.swing.event.*;
-import java.lang.reflect.*;
-import javax.swing.border.*;
-import javax.swing.plaf.basic.*;
-import java.awt.image.ImageConsumer;
-import java.awt.image.ImageObserver;
-import java.applet.AudioClip;
-import javax.swing.filechooser.FileView;
-import java.net.*;
 
+
+@SuppressWarnings("serial")
 class Addcust extends JFrame {
 	JTextField t1, t2, t3, t4, t5, t6;
-	JTextArea a1, a2, a3, a5, a4, a6, a7;
-	JButton b1, b2, b3, b4, b5, b6, b7, b8, b9, b10;
-	JLabel l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11, l12, l13, l14;
-	JPanel jp1, jp2, jp3, jp4, jp5, jp6;
-	FileReader rd1;
-	JTextField read1;
-	FileWriter wr1;
+	JTextArea textArea;
+	JButton registerButton, resetButton, cancelButton;
+	JLabel l1, l2, l3, l4, l5, l6, l7;
+	JPanel panel;
+	FileReader rd;
+	JTextField read;
+	FileWriter wr;
 	String year = "20";
 	Container cp;
 
 	Addcust() {
-		super("Add customer");
-
+		super("Add Customer");
+		try {
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+		}
+		catch (Exception de) {
+		
+		}
+		
+		setSize(330, 350);
+		setLocation(130, 150);
+		setVisible(true);
 		cp = getContentPane();
 		cp.setLayout(null);
 
-		jp1 = new JPanel(null, true);
+		panel = new JPanel(null, true);
 
-		l1 = new JLabel("Customer ID.");
+		// For reference: (x, y, length, height)
+		
+		l1 = new JLabel("Customer ID");
 		l1.setBounds(10, 10, 150, 25);
-		jp1.add(l1);
+		panel.add(l1);
 
 		t1 = new JTextField();
-		t1.setBounds(100, 10, 120, 25);
-		jp1.add(t1);
+		t1.setBounds(130, 10, 160, 25);
+		panel.add(t1);
 
 		l2 = new JLabel("Customer Name");
 		l2.setBounds(10, 40, 150, 25);
-		jp1.add(l2);
+		panel.add(l2);
 
 		Date d = new Date();
-		int date = d.getDate();
-		int mon = d.getMonth();
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(d);
+		int mon = cal.get(Calendar.MONTH);
+		int date = cal.get(Calendar.DATE);
 		mon = mon + 1;
-		int y = d.getYear();
+		int y = cal.get(Calendar.YEAR);
 
 		String str = y + "";
 		int len = str.length();
 
-		for (int i = 0; i < len; i++) {
+		for (int i = 2; i < len; i++) {
 
 			if (i > 0) {
 				year = year + str.charAt(i);
 			}
 		}
-		String issue = date + "/" + mon + "/" + year;
+		String issue = mon + "/" + date + "/" + year;
 
 		t2 = new JTextField();
-		t2.setBounds(100, 40, 120, 25);
-		jp1.add(t2);
+		t2.setBounds(130, 40, 160, 25);
+		panel.add(t2);
 
 		l3 = new JLabel("Registration Date");
 		l3.setBounds(10, 70, 150, 25);
-		jp1.add(l3);
+		panel.add(l3);
 
 		t3 = new JTextField(issue);
 		t3.setEditable(false);
-		t3.setBounds(100, 70, 120, 25);
-		jp1.add(t3);
+		t3.setBounds(130, 70, 160, 25);
+		panel.add(t3);
 
 		l4 = new JLabel("Book Name");
 		l4.setBounds(10, 100, 150, 25);
-		jp1.add(l4);
+		panel.add(l4);
 
 		t4 = new JTextField();
 		t4.setEditable(false);
-		t4.setBounds(100, 100, 120, 25);
-		jp1.add(t4);
+		t4.setBounds(130, 100, 160, 25);
+		panel.add(t4);
 
-		l5 = new JLabel("Date Of Purcase");
+		l5 = new JLabel("Date Of Purchase");
 		l5.setBounds(10, 130, 150, 25);
-		jp1.add(l5);
+		panel.add(l5);
 
 		t5 = new JTextField();
 		t5.setEditable(false);
-		t5.setBounds(100, 130, 120, 25);
-		jp1.add(t5);
+		t5.setBounds(130, 130, 160, 25);
+		panel.add(t5);
 
 		l6 = new JLabel("Date Of Return");
 		l6.setBounds(10, 160, 150, 25);
-		jp1.add(l6);
+		panel.add(l6);
 
 		t6 = new JTextField();
 		t6.setEditable(false);
-		t6.setBounds(100, 160, 120, 25);
-		jp1.add(t6);
+		t6.setBounds(130, 160, 160, 25);
+		panel.add(t6);
 
 		l7 = new JLabel("Details");
 		l7.setBounds(10, 190, 150, 25);
-		jp1.add(l7);
+		panel.add(l7);
 
-		a1 = new JTextArea();
-		a1.setBounds(10, 215, 200, 70);
-		jp1.add(a1);
+		textArea = new JTextArea();
+		textArea.setBounds(130, 190, 160, 70);
+		textArea.setLineWrap(true);
+		panel.add(textArea);
 
-		b1 = new JButton("Register");
-		b1.setBounds(10, 310, 90, 20);
-		jp1.add(b1);
+		registerButton = new JButton("Register");
+		registerButton.setBounds(10, 275, 90, 25);
+		panel.add(registerButton);
 
-		b2 = new JButton("Reset");
-		b2.setBounds(120, 310, 90, 20);
-		jp1.add(b2);
+		resetButton = new JButton("Reset");
+		resetButton.setBounds(110, 275, 90, 25);
+		panel.add(resetButton);
 
-		b3 = new JButton("Cancel");
-		b3.setBounds(230, 310, 90, 20);
-		jp1.add(b3);
+		cancelButton = new JButton("Cancel");
+		cancelButton.setBounds(210, 275, 90, 25);
+		panel.add(cancelButton);
 
-		jp1.setBounds(10, 10, 350, 350);
-		cp.add(jp1);
+		Color r3 = new Color(122, 145, 195);
+		panel.setBackground(r3);
+		panel.setBounds(10, 10, 310, 310);
+		cp.add(panel);
 
-		b2.addActionListener(new ActionListener() {
+		resetButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				t1.setText("");
 				t6.setText("");
 				t4.setText("");
 				t2.setText("");
-				a1.setText("");
+				textArea.setText("");
 				t5.setText("");
 			}
 		});
 
-		b3.addActionListener(new ActionListener() {
+		cancelButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				setVisible(false);
 			}
 		});
 
-		b1.addActionListener(new ActionListener() {
+		registerButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
 					File file = new File("Cust_Details");
@@ -151,6 +159,7 @@ class Addcust extends JFrame {
 						file.mkdir();
 					}
 
+					@SuppressWarnings("unused")
 					File file2 = new File("Cust_Details/pointer.mmm");
 					if (file.isFile() && (!file.exists())) {
 						FileWriter w1 = new FileWriter(
@@ -159,46 +168,46 @@ class Addcust extends JFrame {
 						w1.close();
 					}
 
-					rd1 = new FileReader("Cust_Details/pointer.mmm");
-					read1 = new JTextField();
-					read1.read(rd1, null);
-					rd1.close();
+					rd = new FileReader("Cust_Details/pointer.mmm");
+					read = new JTextField();
+					read.read(rd, null);
+					rd.close();
 
-					int z = Integer.parseInt(read1.getText());
+					int z = Integer.parseInt(read.getText());
 
-					wr1 = new FileWriter("Cust_Details/Cus" + z + ".id");
-					wr1.write(t1.getText() + "");
-					wr1.close();
+					wr = new FileWriter("Cust_Details/Cus" + z + ".id");
+					wr.write(t1.getText() + "");
+					wr.close();
 
-					wr1 = new FileWriter("Cust_Details/Cus" + z + ".name");
-					wr1.write(t2.getText() + "");
-					wr1.close();
+					wr = new FileWriter("Cust_Details/Cus" + z + ".name");
+					wr.write(t2.getText() + "");
+					wr.close();
 
-					wr1 = new FileWriter("Cust_Details/Cus" + z + ".date");
-					wr1.write(t3.getText() + "");
-					wr1.close();
+					wr = new FileWriter("Cust_Details/Cus" + z + ".date");
+					wr.write(t3.getText() + "");
+					wr.close();
 
-					wr1 = new FileWriter("Cust_Details/Cus" + z + ".bname");
-					wr1.write(t4.getText() + "");
-					wr1.close();
+					wr = new FileWriter("Cust_Details/Cus" + z + ".bname");
+					wr.write(t4.getText() + "");
+					wr.close();
 
-					wr1 = new FileWriter("Cust_Details/Cus" + z + ".purchase");
-					wr1.write(t5.getText() + "");
-					wr1.close();
+					wr = new FileWriter("Cust_Details/Cus" + z + ".purchase");
+					wr.write(t5.getText() + "");
+					wr.close();
 
-					wr1 = new FileWriter("Cust_Details/Cus" + z + ".return");
-					wr1.write(t6.getText() + "");
-					wr1.close();
+					wr = new FileWriter("Cust_Details/Cus" + z + ".return");
+					wr.write(t6.getText() + "");
+					wr.close();
 
-					wr1 = new FileWriter("Cust_Details/Cus" + z + ".detail");
-					wr1.write(a1.getText() + "");
-					wr1.close();
+					wr = new FileWriter("Cust_Details/Cus" + z + ".detail");
+					wr.write(textArea.getText() + "");
+					wr.close();
 
 					z = z + 1;
 
-					wr1 = new FileWriter("Cust_Details/pointer.mmm");
-					wr1.write(z + "");
-					wr1.close();
+					wr = new FileWriter("Cust_Details/pointer.mmm");
+					wr.write(z + "");
+					wr.close();
 
 					setVisible(false);
 
